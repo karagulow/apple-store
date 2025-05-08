90d0195c812fa03a36a4ae934e42c7fb5f61a45f
Albert <albert.karagulov@mail.ru>
2025-05-08 21:23:32 +0300

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { AppRouter } from './routers/app-router';
+
+// import './styles/main.scss';
+
+const App: React.FC = () => {
+	return (
+		<div className='app'>
+			<AppRouter />
+		</div>
+	);
+};
+
+export default App;
