1dbdce991064260bc372c0735504a65729434db2
Albert Karagulov <albert.karagulov@mail.ru>
2025-07-04 09:49:58 +0300

    refactor: optimize header component

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,3 +1,4 @@
+import { memo, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../../shared/lib/hooks/redux';
 
@@ -7,28 +8,41 @@ import bagIcon from '../../../shared/assets/images/bag.svg';
 
 import { Container, Logo } from '../../../shared/ui';
 
+const LogoLink = memo(() => (
+	<Link className={styles.header__logo} to='/'>
+		<Logo />
+	</Link>
+));
+
+const FavoriteLink = memo(() => (
+	<Link className={styles.header__link} to='/favorites'>
+		<img src={heartIcon} alt='heart' aria-label='Перейти в избранное' />
+	</Link>
+));
+
+const CartLink = memo(({ totalItems }: { totalItems: number }) => (
+	<Link className={styles.header__link} to='/cart'>
+		<img src={bagIcon} alt='bag' aria-label='Перейти в корзину' />
+		{totalItems > 0 && <div className={styles.header__count}>{totalItems}</div>}
+	</Link>
+));
+
 export const Header: React.FC = () => {
 	const cart = useAppSelector(state => state.cart);
-	const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+	const totalItems = useMemo(
+		() => cart.reduce((sum, item) => sum + item.quantity, 0),
+		[cart]
+	);
 
 	return (
 		<header className={styles.header}>
 			<Container>
 				<div className={styles.header__wrapper}>
-					<Link to='/'>
-						<Logo />
-					</Link>
+					<LogoLink />
 
 					<div className={styles.header__links}>
-						<Link className={styles.header__link} to='/favorites'>
-							<img src={heartIcon} alt='heart' />
-						</Link>
-						<Link className={styles.header__link} to='/cart'>
-							<img src={bagIcon} alt='bag' />
-							{totalItems > 0 && (
-								<div className={styles.header__count}>{totalItems}</div>
-							)}
-						</Link>
+						<FavoriteLink />
+						<CartLink totalItems={totalItems} />
 					</div>
 				</div>
 			</Container>
